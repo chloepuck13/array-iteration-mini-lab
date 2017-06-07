5c1cdf397646120938779c8de06aed57e5932397
Chloe Marie <chloepuck13@yahoo.com>
2017-06-07 17:03:12 +0000

    Array Iteration Mini Lab Finished

---
+++ array-mini-lab.rb
@@ -0,0 +1,17 @@
+farmer_tasks = ["Milk the cow", "Churn the butter", "Drive the tractor", "Peel the potatoes", "Plant the corn"]
+farmer_tasks.each do |task|
+    puts "I'm so busy! I need to " + task
+end
+
+some_numbs = [2, 52, 19, 46, 1000]
+some_numbs.each do |numbs|
+    puts (numbs+10)/2
+end
+presidents = ["George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy Adams"]
+presidents.each do |presidents|
+    puts presidents.reverse
+end
+milk = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+milk.each do |wall|
+    puts "#{wall} bottles of milk on the wall"
+end
